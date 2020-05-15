e742caa4d32aaa9b1d42bcb32df63860d0c3a77f
snorlexK <now97kim@naver.com>
2020-05-15 12:50:52 +0900

    #1 naver 스파이더 생성

--- noreunja/noreunja/items.py
+++ noreunja/noreunja/items.py
@@ -11,4 +11,13 @@ import scrapy
 class NoreunjaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+
+    # 검색어
+    keyword = scrapy.Field()
+
+    # 데이터 수집 시의 실시간 순위
+    rank = scrapy.Field()
+
+    # 데이터가 수집된 시간
+    time = scrapy.Field()
     pass
