d5a7d30c827514bb75aaf288fc79f3974d926309
hem22 <reabt12@gmail.com>
2020-05-22 12:47:31 +0900

    #3 네이버 크롤링 문제  DEBUG: Redirecting (301) to <GET http://datalab.naver.com/notfound.html> DEBUG: Crawled (200) <GET http://datalab.naver.com/notfound.html> 파일을 찾을 수 없다고 나옴
    Signed-off-by: hem22 <reabt12@gmail.com>

--- noreunja/noreunja/items.py
+++ noreunja/noreunja/items.py
@@ -12,12 +12,22 @@ class NoreunjaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
 
-    # 검색어
-    keyword = scrapy.Field()
-
-    # 데이터 수집 시의 실시간 순위
-    rank = scrapy.Field()
+    # 순위 - 키워드
+    naver_01 = scrapy.Field()
+    naver_02 = scrapy.Field()
+    naver_03 = scrapy.Field()
+    naver_04 = scrapy.Field()
+    naver_05 = scrapy.Field()
+    naver_06 = scrapy.Field()
+    naver_07 = scrapy.Field()
+    naver_08 = scrapy.Field()
+    naver_09 = scrapy.Field()
+    naver_10 = scrapy.Field()
 
     # 데이터가 수집된 시간
-    time = scrapy.Field()
+    google_time = scrapy.Field()
+    naver_time = scrapy.Field()
+    nate_time = scrapy.Field()
+    zum_time = scrapy.Field()
+
     pass
--- noreunja/noreunja/spiders/naver_crawl.py
+++ noreunja/noreunja/spiders/naver_crawl.py
@@ -6,16 +6,36 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class NaverCrawlSpider(CrawlSpider):
     name = 'naver_crawl'
-    allowed_domains = ['naver.com']
-    start_urls = ['http://naver.com/']
+    allowed_domains = ['datalab.naver.com']
+    start_urls = ['https://datalab.naver.com/keyword/realtimeList.naver']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'\?age=all&entertainment=-2&groupingLevel=0&marketing=-2&news=-2&sports=-2'),
+             callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        i = {}
+        i['naver_01'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[1]/div/span[2]/span').extract()
+        i['naver_02'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[2]/div/span[2]/span').extract()
+        i['naver_03'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[3]/div/span[2]/span').extract()
+        i['naver_04'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[4]/div/span[2]/span').extract()
+        i['naver_05'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[5]/div/span[2]/span').extract()
+        i['naver_06'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[6]/div/span[2]/span').extract()
+        i['naver_07'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[7]/div/span[2]/span').extract()
+        i['naver_08'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[8]/div/span[2]/span').extract()
+        i['naver_09'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[9]/div/span[2]/span').extract()
+        i['naver_10'] = response.xpath(
+            '//*[@id="content"]/div/div[2]/div[2]/div[2]/div/div/ul[1]/li[10]/div/span[2]/span').extract()
+
+        return i
+
