2417593c500d5b8dba1df2d7b1d3b4638d558064
snorlexK <now97kim@naver.com>
2020-05-29 11:09:42 +0900

    #5 네이트 검색어 크롤링

---
+++ nate_crawl.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import os
+from datetime import datetime
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+req = requests.get('http://ad.search.nate.com/iframe/bizhotkwd.html?v=2')
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+
+keywords = soup.select('a')
+
+data = []
+
+for item in keywords:
+    data.append(
+        {
+            'keyword': item.text,
+            'time': str(datetime.now()),
+        }
+    )
+
+print(data)
+
+data = pd.DataFrame(data)
+data.to_csv('nate_list.csv')
