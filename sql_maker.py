d0a333a1f90685beacb1eec71677da6db28f842f
HyeonjaeKim <now97kim@naver.com>
2020-06-19 02:11:21 +0900

    #7 크롤링 데이터 -> SQL 파일로 스프링 프로젝트에 저장

--- sql_maker.py
+++ sql_maker.py
@@ -11,23 +11,23 @@ for item in googleCsv.iloc[:].values:
     query = "INSERT INTO google (rank, keyword, time) VALUES (%d, '%s', '%s')\n" % (item[0], item[1], time)
     queryList += query
 
-# queryList += "\n"
+queryList += "\n"
 
 for item in nateCsv.iloc[:].values:
     time = item[2][:-7]
     query = "INSERT INTO nate (rank, keyword, time) VALUES (%d, '%s', '%s')\n" % (item[0], item[1], time)
     queryList += query
 
-# queryList += "\n"
+queryList += "\n"
 
 for item in zumCsv.iloc[:].values:
     time = item[2][:-7]
     query = "INSERT INTO zum (rank, keyword, time) VALUES (%d, '%s', '%s')\n" % (item[0], item[1], time)
     queryList += query
 
-# queryList += "\n"
+queryList += "\n"
 
-# f = open("../2020_1_Python_Noreunja_Spring/src/main/resources/data.sql", "a", encoding="utf-8")
-f = open("data.sql", "a", encoding="utf-8")
+f = open("../2020_1_Python_Noreunja_Spring/src/main/resources/data.sql", "a", encoding="utf-8")
+# f = open("data.sql", "a", encoding="utf-8")
 f.write(queryList)
 f.close()
